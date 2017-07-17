865b62758e8bd8ab2154c301598a52bdd5840983
Emerson Silvestrin <emersonsilvestrin@live.com>
2017-07-17 15:13:09 -0300

    commit #7
    iniciado pagina de grava cad funcionario
    precisa ser testado e ajustado

--- conexao/grava-cliente.php
+++ conexao/grava-cliente.php
@@ -5,10 +5,6 @@ if((!isset ($_SESSION['id']) == true) and (!isset ($_SESSION['login']) == true))
 {
     header('location:../login/logar.html');
 }
-$id_user = $_SESSION['id'];
-$logado = $_SESSION['login'];
-$acesso = $_SESSION['acesso'];
-
 // include do arquivo de conexao do banco de dados
 require '../conexao/bd/conecta.php';
 
@@ -22,5 +18,25 @@ $fone = $_POST['user-fone'];
 $cidade = $_POST['cid-cidade'];
 $bairro = $_POST['id-bairro'];
 
+//inicia validação e inserção dos dados
+if(!empty($nome) && !empty($sobrenome) && !empty($email) && !empty($fone) && !empty($cidade) && !empty($bairro)){ //verifica se todos os dados obrigatorios foram preenchidos
+    $sql = "INSERT INTO cli_cliente (Cli_nome, Cli_sobrenome, Cli_email, Cli_endereco, Cli_complemento, Cli_telefone, Cid_cidade_Cid_id, Br_Bairro_Br_id) 
+            VALUES ('$nome' ,'$sobrenome' ,'$email' ,'$email' ,'$endereco' ,'$complemento' ,'$fone' ,'$cidade' ,'$bairro')";
+    //$consulta_query = "";
+    //executa a query de consulta para linha e horario;
+    $result_query = mysqli_query($conn,$sql);
+    if(mysqli_num_rows ($result_query) > 0 ) {  // retorno da consulta de horario e linha;
+        echo "New record created successfully";
+    }else{ // inicia query para gravar no banco
+        header("HTTP/1.0 401");
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+}else{
+    header("HTTP/1.0 400 Por Favor Preencha todos os Dados Corretamente!");
+    //echo "<script> alert('Por Favor Preencha todos os Dados Corretamente!'); </script>";
+}
+//}//fim do else !$conn
 
+mysqli_close($conn);//encera conexao com o banco
+?>
 
